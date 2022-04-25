564e29e9441274a45d6cb78d11961fd81b3b5d03
ozanpolat48 <mrozanpolat@gmail.com>
2022-04-25 23:32:19 +0300

    edited some changes

---
+++ src/day42_Exceptions/ThrowKeyword2.java
@@ -0,0 +1,21 @@
+package day42_Exceptions;
+
+import java.util.NoSuchElementException;
+
+public class ThrowKeyword2 {
+
+
+    public static void main(String[] args) {
+
+
+        throw new NoSuchElementException("No such Element");
+        //throw new InputMismatchException();
+
+
+        //System.out.println("hello world");
+
+        //throw new Rectangle(5,7);
+
+
+    }
+}
