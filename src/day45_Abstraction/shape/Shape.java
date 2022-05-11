2d3257c271e5a6e2287b5905c018e7fe3b4ee993
ozanpolat48 <mrozanpolat@gmail.com>
2022-05-11 15:05:12 +0300

    edited some changes

--- src/day45_Abstraction/shape/Shape.java
+++ src/day45_Abstraction/shape/Shape.java
@@ -18,10 +18,10 @@ public abstract class Shape {
 
     @Override
     public String toString() {
-        return getClass().getSimpleName()+"{" +
+        return
                 "name='" + name + '\'' +
                 ", area='" + area() + '\'' +
-                ", perimeter='" + perimeter() + '\'' +
-                '}';
+                ", perimeter='" + perimeter() + '\'' ;
+
     }
 }
