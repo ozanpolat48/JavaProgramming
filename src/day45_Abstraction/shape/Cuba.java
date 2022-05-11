c7e88ce6d965eafa58715fc7187112b3933ae29d
ozanpolat48 <mrozanpolat@gmail.com>
2022-05-11 15:15:22 +0300

    edited some changes

--- src/day45_Abstraction/shape/Cuba.java
+++ src/day45_Abstraction/shape/Cuba.java
@@ -1,4 +1,19 @@
 package day45_Abstraction.shape;
 
-public class Cuba {
+public class Cuba extends Shape {
+
+
+    public Cuba(String name) {
+        super(name);
+    }
+
+    @Override
+    public double area() {
+        return 0;
+    }
+
+    @Override
+    public double perimeter() {
+        return 0;
+    }
 }
